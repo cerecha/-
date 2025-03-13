4d96ac336b32bd07faf8dbd0dd2749b48db5f3f7
cerecha <Aakaashmj91@gmail.com>
2025-03-13 13:20:36 +0900

    [Bronze V] Title: 사칙연산, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.py
@@ -0,0 +1,17 @@
+
+a = list(map(int,input().split()))
+
+num1 = a[0] + a[1]
+num2 = a[0] - a[1]
+num3 = a[0] * a[1]
+num4 = a[0] // a[1]
+num5 = a[0] % a[1]
+
+
+print(num1)
+print(num2)
+print(num3)
+print(num4)
+print(num5)
+
+
