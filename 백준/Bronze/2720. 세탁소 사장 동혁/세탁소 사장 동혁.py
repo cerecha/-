dcd5c2c214c0bab2622327cc2931052fa82e1fe9
cerecha <Aakaashmj91@gmail.com>
2025-04-08 13:06:57 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 60 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -0,0 +1,13 @@
+
+coin = [25, 10, 5, 1]
+T = int(input())
+
+for _ in range(T) :
+    C = int(input())
+    S = []
+
+    for i in coin :
+        S.append(C // i)
+        C = C % i	
+        
+    print(*S)
