eb53196d636bcdd3de31e5c6fb16ec6c48a34c63
cerecha <Aakaashmj91@gmail.com>
2025-04-09 13:52:57 +0900

    [Bronze III] Title: 세 막대, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/14215. 세 막대/세 막대.py
@@ -0,0 +1,16 @@
+
+import sys
+input= sys.stdin.readline
+
+a, b, c = map(int, sys.stdin.readline().split())
+
+MAX = max(a, b, c)
+SUM = (a + b + c) - MAX
+
+if (MAX < SUM):
+    print(a + b + c)
+else:
+    MAX = SUM - 1
+    print(SUM + MAX)
+
+    
