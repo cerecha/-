8bc19c999197a1de633d6fd18e8b7629d2bb8fcd
cerecha <Aakaashmj91@gmail.com>
2025-04-30 16:56:20 +0900

    [Silver V] Title: 영화감독 숌, Time: 668 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,19 @@
+
+import sys
+input= sys.stdin.readline
+
+n =  int(input())
+c = 0
+set = 666
+
+while True:
+    if '666' in str(set):
+        c += 1
+    
+    if c == n :
+        break
+    
+    set += 1
+    
+print(set)
+        
